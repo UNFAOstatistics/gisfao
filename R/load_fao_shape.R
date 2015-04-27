ef97cf2e0f109973fe869db28884e34bfafb9f1b
Markus Kainu <markus.kainu@fao.org>
2015-04-27 13:57:55 +0200

    Revised the whole package and added a centroid data.
    not a single function left, only data

--- R/load_fao_shape.R
+++ R/load_fao_shape.R
@@ -6,11 +6,11 @@
 #' africa <- load_fao_shape(region = "RAF")
 #' plot(africa)
 
-load_fao_shape <- function(region="world") {
-  load("data/fao_world.RData")
-  if (region == "world") shape <- fao_world
-  if (region != "world") shape <- fao_world[which(fao_world[[region]] == TRUE),]
-  shape
-}
-
+# load_fao_shape <- function(region="world") {
+#   load("data/fao_world.RData")
+#   if (region == "world") shape <- fao_world
+#   if (region != "world") shape <- fao_world[which(fao_world[[region]] == TRUE),]
+#   shape
+# }
+#
 
